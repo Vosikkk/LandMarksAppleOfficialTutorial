b92715664b82590cabba4c133dd6734e9defdc31
Vosik <vosikvos@gmail.com>
2025-03-16 21:52:10 +0200

    F!! - add favorite and filter for list

--- LandMarks/View/LandmarkList.swift
+++ LandMarks/View/LandmarkList.swift
@@ -9,18 +9,33 @@ import SwiftUI
 
 struct LandmarkList: View {
     
+    @State private var showFavoritesOnly: Bool = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter {
+            (!showFavoritesOnly || $0.isFavorite)
+        }
+    }
+    
     var body: some View {
         
         NavigationSplitView {
             
-            List(landmarks) { landmark in
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
                 
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+                ForEach(filteredLandmarks) { landmark in
+                    
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
             
         } detail: {
