d41a8f045d233824c8dc59c85a9efbcb86ac21dd
Vosik <vosikvos@gmail.com>
2025-03-16 15:21:43 +0200

    e - add gitignore

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Саша Восколович on 16.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Turtle Rock!")
+                .font(.title)
+                .foregroundStyle(.green)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Саша Восколович on 16.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
